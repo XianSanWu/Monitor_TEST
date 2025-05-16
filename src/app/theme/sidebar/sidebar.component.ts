2eee353d7846e0c9f815b895721c35c8766e55ab
泓賢 吳 <ztex11020676522@gmail.com>
2025-05-16 11:32:15 +0800

    頁面控顯示改至config

--- src/app/theme/sidebar/sidebar.component.ts
+++ src/app/theme/sidebar/sidebar.component.ts
@@ -7,45 +7,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { LocalStorageService } from '../../core/services/local-storage.service';
 import { MenuItem } from '../../core/models/common/menu.model';
-
-export const OtherMenu: MenuItem[] = [
-  {
-    title: "CDP工作流程管理", icon: "list", link: '',
-    children: [
-      { title: "EDM", icon: "envelope-open-fill", link: "/cdp/edm_main", children: [] },
-      { title: "SMS", icon: "chat-fill", link: "/cdp/sms_main", children: [] },
-      { title: "APP_PUSH", icon: "bell-fill", link: "/cdp/app_push_main", children: [] },
-    ]
-  },
-  {
-    title: "MSMQ", icon: "list", link: '',//Microsoft Message Queuing
-    children: [
-      { title: "訊息佇列系統", icon: "gear", link: "/msmq/queue", children: [] },
-    ]
-  },
-
-];
-
-export const AdminMenu: MenuItem[] = [
-  { title: "首頁管理", icon: "house-door", link: "/home", children: [] },
-  //#region 測試
-  { title: "登入管理", icon: "lock", link: "/login", children: [] },
-  {
-    title: "測試管理", icon: "gear", link: '',
-    children: [
-      { title: "測試1", icon: "file-earmark-text", link: "/test/test1", children: [] },
-      { title: "測試2", icon: "file-earmark-text", link: "/test/test2", children: [] },
-    ]
-  },
-  //#endregion
-  ...OtherMenu,
-  {
-    title: "Mail Hunter 報表", icon: "list", link: '',
-    children: [
-      { title: "年度統計報表", icon: "file-earmark-text-fill", link: "/mailhunter/project_mail_count", children: [] },
-    ]
-  },
-];
+import { ConfigService } from '../../core/services/config.service';
 
 @Component({
   selector: 'sidebar',
@@ -68,16 +30,27 @@ export class SidebarComponent implements OnInit {
   @Output() menuItemClicked = new EventEmitter<void>();
 
   menuItems!: MenuItem[];
+  testMenu!: MenuItem[];
+  adminMenu!: MenuItem[];
+  otherMenu!: MenuItem[];
 
-  constructor(private localStorageService: LocalStorageService,
-    private cd: ChangeDetectorRef
-  ) {}
+  constructor(
+    private localStorageService: LocalStorageService,
+    private cd: ChangeDetectorRef,
+    private configService: ConfigService,
+  ) {
+    this.configService.configData$.subscribe(data => {
+      this.testMenu = data?.testMenu;
+      this.otherMenu = data?.otherMenu;
+      this.adminMenu = [...(data?.adminMenu ?? []), ...(this?.otherMenu ?? []), ...(this.testMenu ?? [])];
+    });
+  }
 
   ngOnInit(): void {
     // console.log('SSSS.isSmallScreen', this.isSmallScreen);
     // console.log('SSSS.opened', this.opened);
     const user = this.localStorageService.getItem('isLoggedIn')?.toLocaleLowerCase();
-    this.menuItems = (user === 'admin') ? Object.values(AdminMenu) : Object.values(OtherMenu);
+    this.menuItems = (user === 'admin') ? Object.values(this.adminMenu) : Object.values(this.otherMenu);
     // console.log('SSSS.user', user);
     // console.log('SSSS.menuItems', this.menuItems);
     this.cd.detectChanges();
